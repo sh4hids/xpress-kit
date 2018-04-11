9b403cbae3c7658ffef4d0312414b59ceed050a1
5h4hid5 <shahid.bracu@gmail.com>
2018-04-11 06:37:45 +0600

    Updated gitingore

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+const keys = {
+  google: {
+    clientID: '174365168645-8a33nnfev3o79a3hbavrasmplkntrmh9.apps.googleusercontent.com',
+    clientSecret: 'DVNIA167G7TjLxqiedYs-AiG'
+  }
+}
+
+module.exports = keys;

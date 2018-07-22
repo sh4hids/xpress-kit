57ce5ae5c26127e8174fd29e6766a3580f8a94c2
sh4hids <hello@shahid.pro>
2018-07-22 21:49:36 +0600

    :zap: Updated packages and added keys example

---
+++ app/config/example.keys.js
@@ -0,0 +1,24 @@
+const keys = {
+  mongodb: 'db-url-here',
+  github: {
+    clientID: 'sdfsadf',
+    clientSecret: 'sdfsaf',
+  },
+  facebook: {
+    clientID: '',
+    clientSecret: '',
+  },
+  twitter: {
+    clientID: '',
+    clientSecret: '',
+  },
+  google: {
+    clientID: '',
+    clientSecret: '',
+  },
+  session: {
+    cookieSecret: 'sdhfsadfsaf',
+  },
+};
+
+module.exports = keys;
